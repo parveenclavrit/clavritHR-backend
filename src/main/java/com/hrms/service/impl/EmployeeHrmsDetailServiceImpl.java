e83f2b8965c6d5cb004b1f47ceae6ce48c5151c0
meenakshichhaunker <meenakshi@clarvit.com>
2022-07-28 13:35:51 +0530

    fixed conflict issues

--- src/main/java/com/hrms/service/impl/EmployeeHrmsDetailServiceImpl.java
+++ src/main/java/com/hrms/service/impl/EmployeeHrmsDetailServiceImpl.java
@@ -1,11 +1,13 @@
 package com.hrms.service.impl;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.hrms.dto.EmployeeHrmsDetailDto;
 import com.hrms.entity.EmployeeHrmsDetail;
 import com.hrms.repository.EmployeeHrmsRepository;
 import com.hrms.service.EmployeeHrmsDetailService;
@@ -25,12 +27,23 @@ public class EmployeeHrmsDetailServiceImpl implements EmployeeHrmsDetailService 
 	}
 
 	@Override
-	public EmployeeHrmsDetail saveEmployeeMaster(EmployeeHrmsDetail employeeHrmsDetail) {
-		 return this.eRepo.save(employeeHrmsDetail);
+	@Transactional
+	public EmployeeHrmsDetail saveEmployeeHrmsDetails(Date currentDate, EmployeeHrmsDetailDto hrmsDto) {
+		EmployeeHrmsDetail e2 = new EmployeeHrmsDetail();
+		e2.setCasual_leaves(hrmsDto.getCasual_leaves());
+		e2.setCreated_on(currentDate);
+		e2.setDepartment(hrmsDto.getDepartment());
+		e2.setDoj(hrmsDto.getDoj());
+		e2.setEarn_leaves(hrmsDto.getEarn_leaves());
+		e2.setEmail(hrmsDto.getEmail());
+		e2.setSick_leaves(hrmsDto.getSick_leaves());
+		e2.setUpdated_on(currentDate);
+		return this.saveEmployeeHrmsDetails(e2);
 	}
 
-
- 
-	 
+	@Transactional
+	protected EmployeeHrmsDetail saveEmployeeHrmsDetails(EmployeeHrmsDetail employeeHrmsDetail) {
+		return this.eRepo.save(employeeHrmsDetail);
+	}
 
 }
