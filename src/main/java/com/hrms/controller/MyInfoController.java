60df5be2dc8dac54c50ac5372d6d4790cc18a53f
parveenclavrit <102028761+parveenclavrit@users.noreply.github.com>
2022-09-19 19:39:35 +0530

    fix issues in MyInfo page

--- src/main/java/com/hrms/controller/MyInfoController.java
+++ src/main/java/com/hrms/controller/MyInfoController.java
@@ -8,11 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.hrms.entity.EmployeeHiringDetail;
+import com.hrms.entity.MyInfoDetail;
 import com.hrms.service.MyInfoService;
 
 @RestController
@@ -23,6 +27,10 @@ public class MyInfoController {
 	@Value("${file.upload-dir}")
 	String FILE_DIRECTORY;
 
+	@GetMapping("/getMyInfo/{Id}")
+	   public MyInfoDetail getDetail(@PathVariable String Id) {
+		   
+		   return this.myinfoser.getMyInfoDetail(Integer.parseInt(Id));}
 	
 
 	@PostMapping(value = "/uploadFile", consumes = "multipart/form-data")
